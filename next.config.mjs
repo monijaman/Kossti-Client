49d36bbf8354052a148533877e40f7542171d62c
Monir <monir0space@gmail.com>
2024-11-19 11:19:45 +0600

    next

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  i18n: {
-    locales: ['en', 'bn', 'fr', 'es', 'de'], // Add your supported locales here
-    defaultLocale: 'en', // Set your default locale
-    localeDetection: true, // Automatically detect user locale (default is true)
-  },
+  // i18n: {
+  //   locales: ['en', 'bn', 'fr', 'es', 'de'], // Add your supported locales here
+  //   defaultLocale: 'en', // Set your default locale
+  //   localeDetection: false, // Automatically detect user locale (default is true)
+  // },
   images: {
     domains: ['127.0.0.1', 'localhost', 'viper.monirspace.com'], // Add your local domains here
   },
