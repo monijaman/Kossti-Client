9bb5e6cbf6d04e63e3bbb817d8f43db746d0a48c
Monir <monir0space@gmail.com>
2026-02-06 18:22:14 +0600

    analytics added

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -2,6 +2,7 @@
 import ClientProvider from '@/app/components/Provider/ClientProvider';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import Script from 'next/script';
 import { ReactNode } from 'react';
 // import { LanguageProvider } from '../context/LanguageContext';
 import '../globals.scss';
@@ -25,6 +26,20 @@ export default async function RootLayout({ children, params }: RootLayoutProps) 
 
   return (
     <html lang={locale}>
+      <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-8JP2X3MRQ5"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-8JP2X3MRQ5');
+          `}
+        </Script>
+      </head>
       <body className={inter.className}>
         {/* <LanguageProvider> */}
         <ClientProvider>{children}</ClientProvider>
