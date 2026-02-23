458a69ecc40f60b3112c71c8ab68a2b8833cf9d7
Monir <monir0space@gmail.com>
2026-02-23 17:00:01 +0600

    env

--- src/app/components/Auth/CheckAuth.tsx
+++ src/app/components/Auth/CheckAuth.tsx
@@ -1,11 +1,11 @@
 import { cookies } from "next/headers";
 
-const CheckAuth = async () =>  {
-    
+const CheckAuth = async () => {
+
     const countryCode = (await cookies()).get('country-code')?.value;
 
- 
-    console.log("countryCode:", countryCode); // Log the value to the console
+
+    console.log("countryCode - :", countryCode); // Log the value to the console
     return countryCode;
 };
 
