7eb96f3f745817ba2696db6c804c2f7bb26d13ca
Monir <monir0space@gmail.com>
2026-05-29 14:04:07 +0600

    updates

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  optimizePackageImports: ['lucide-react'],
 
   // Optimize production builds
   compress: true,
